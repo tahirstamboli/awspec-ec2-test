3b4c2547d6abe2a8459dc7300e1bc4adf0b5cdf0
tahirstamboli <tahir.tamboli@reancloud.com>
2017-01-20 16:14:25 +0530

    attributes json is added

--- spec/ec2_spec.rb
+++ spec/ec2_spec.rb
@@ -1,20 +1,30 @@
 require 'spec_helper'
 
-describe ec2('i-01fbc3d9871181687') do
+instanceId = getAttr('instanceId')
+imageId = getAttr('imageId')
+instanceNameTag = getAttr('instanceNameTag')
+vpcId = getAttr('vpcId')
+subnetId = getAttr('subnetId')
+securityGroupId = getAttr('securityGroupId')
+securityGroupName = getAttr('securityGroupName')
+privateIp = getAttr('privateIp')
+publicIp = getAttr('publicIp')
+
+describe ec2(instanceId) do
   it { should exist }
   it { should be_running }
-  it { should have_tag('Name').value('TestNow_TST') }
-  its(:instance_id) { should eq 'i-01fbc3d9871181687' }
-  its(:image_id) { should eq 'ami-30e65350' }
-  its(:public_ip_address) { should eq '35.167.195.204' }
-  its(:private_ip_address) { should eq '172.31.12.62' }
+  it { should have_tag('Name').value(instanceNameTag) }
+  its(:instance_id) { should eq instanceId }
+  its(:image_id) { should eq imageId }
+  its(:public_ip_address) { should eq publicIp }
+  its(:private_ip_address) { should eq privateIp }
   it { should be_disabled_api_termination }
-  it { should have_security_group('sg-b004d8c8') }
-  it { should have_security_group('launch-wizard-109') }
+  it { should have_security_group(securityGroupId) }
+  it { should have_security_group(securityGroupName) }
   #it { should have_security_group('my-security-group-tag-name') }
-  it { should belong_to_vpc('vpc-c98a75ac') }
+  it { should belong_to_vpc(vpcId) }
   #it { should belong_to_vpc('my-vpc') }
-  it { should belong_to_subnet('subnet-7b010f3d') }
+  it { should belong_to_subnet(subnetId) }
   #it { should belong_to_subnet('my-subnet') }
   it { should have_eip }
   #it { should have_eip('123.0.456.789') }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 require 'awspec'
+require 'json'
+
+ATTR_STRING  = File.read('spec/attributes.json')
+PARSED_ATTR  = JSON.parse(ATTR_STRING)
+
+def getAttr(key)
+	PARSED_ATTR[key]
+end
