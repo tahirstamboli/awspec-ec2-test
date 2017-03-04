4eda3a4ef5153cbdfbe99da1665e9ed750c71f20
tahirstamboli <tahir.tamboli@reancloud.com>
2017-03-04 16:35:26 +0530

    Report formatting is added in rakefile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'awspec'
 require 'json'
 
+Awsecrets.load(secrets_path: File.expand_path('./secrets.yml', File.dirname(__FILE__)))
+
 ATTR_STRING  = File.read('spec/attributes.json')
 PARSED_ATTR  = JSON.parse(ATTR_STRING)
 
