9f44ec5ddc26cc4865d18d1aa8db54f68653b2bb
Tahir Tamboli <tahirstamboli@hotmail.com>
2017-10-03 18:32:54 +0530

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'awspec'
 require 'json'
 
-Awsecrets.load(secrets_path: File.expand_path('./secrets.yml', File.dirname(__FILE__)))
+#Awsecrets.load(secrets_path: File.expand_path('./secrets.yml', File.dirname(__FILE__)))
 
 ATTR_STRING  = File.read('spec/attributes.json')
 PARSED_ATTR  = JSON.parse(ATTR_STRING)
